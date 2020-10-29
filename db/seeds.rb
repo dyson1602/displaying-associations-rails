e373b8dd6eedebeeda0592e5ba8290ca215cc36a
dyson1602 <cclark000@citymail.cuny.edu>
2020-10-29 09:29:08 -0400

    solved

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+author1 = Author.create(name: "Hunter", genre: "reporting", bio: "whuhiguhdighdf")
+
+category1 = Category.create(name: "article")
+
+Post.create(title: "fear and loathing", description: "crazy story", author_id: author1.id, category_id: category1.id)
